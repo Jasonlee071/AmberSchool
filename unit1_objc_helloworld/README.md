fb8adb8c3e00cd8aa45ca86c09ca37b3107b8097
Jasonlee <jason.lee@ambernet.com.tw>
2019-11-15 14:35:54 +0800

    新增程式碼及調整ReadMe
    1、新增button程式碼
    2、調整label樣式

--- unit1_objc_helloworld/README.md
+++ unit1_objc_helloworld/README.md
@@ -6,7 +6,7 @@
 3. Objective-C - 開發語言，常用檔案.h .m及.mm
 4. Cocoa - iOS的執行框架，類似.net之於C#，最常見就是Foundation及UIKit
 
-![image](https://github.com/Jasonlee071/AmberSchool/blob/master/cocoa.png)
+![Cocoa](https://github.com/Jasonlee071/AmberSchool/blob/master/cocoa.png)
 
 5. Core Data(資料)、Core Graphics(圖形)、Core Animation(動畫)、OpenGL ES vs Metal(繪圖)
 
@@ -15,10 +15,19 @@
 2. 到App Store下載Xcode
 3. 建立新專案
 
-![image](https://github.com/Jasonlee071/AmberSchool/blob/master/createProj.png)
+![Create Proj](https://github.com/Jasonlee071/AmberSchool/blob/master/createProj.png)
 
-![image](https://github.com/Jasonlee071/AmberSchool/blob/master/chooseSingleView.png)
+![Choose Single View](https://github.com/Jasonlee071/AmberSchool/blob/master/chooseSingleView.png)
 
-![image](https://github.com/Jasonlee071/AmberSchool/blob/master/settingProj.png)
+![Setting Proj](https://github.com/Jasonlee071/AmberSchool/blob/master/settingProj.png)
+
+## 編寫程式碼 - Hello World
+1. UIView - 視圖
+2. UILabel - 文字顯示
+3. UIButton - 按鈕
+
+#### **NSLog** - Obj-C廣泛用來看log的函式，但頗吃效能
+
+用上述元件簡單建立一個，點擊按鈕顯示Hello World的iOS App
 
 
--- unit1_objc_helloworld/unit1_objc_helloworld/ViewController.m
+++ unit1_objc_helloworld/unit1_objc_helloworld/ViewController.m
@@ -9,10 +9,12 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (nonatomic, strong) UILabel *label;
 
 @end
 
 @implementation ViewController
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -20,11 +22,53 @@
     NSLog(@"ViewController:viewDidLoad");
     //UILabel *label = [[UILabel alloc] init];
     //[label setFrame:CGRectMake(0, 0, 400, 100)];
-    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 400, 100)];
-    label.text = @"Hello World!!! Amber AppTeam";
-    [self.view addSubview:label];
+    CGFloat viewWidth = self.view.frame.size.width;
+    CGFloat viewHeight = self.view.frame.size.height;
+    self.label = [[UILabel alloc] initWithFrame:CGRectMake((viewWidth - 300) / 2, (viewHeight - 40) / 2, 300, 40)];
+    self.label.textAlignment = NSTextAlignmentCenter;
+    [self.label setBackgroundColor:[UIColor lightTextColor]];
+    self.label.layer.borderColor = [[UIColor blackColor] CGColor];
+    self.label.layer.borderWidth = 1.0f;
+    self.label.layer.cornerRadius = 5.0f;
+    
+    UIButton *button = [[UIButton alloc] initWithFrame:CGRectMake((viewWidth - 150) / 2, CGRectGetMaxY(self.label.frame) + 20 , 150, 44)];
+    [button setTitle:@"請點我" forState:UIControlStateNormal];
+    [button setTitle:@"恢復" forState:UIControlStateSelected];
+    [button setBackgroundColor:[UIColor whiteColor]];
+    [button setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
+    button.layer.borderColor = [[UIColor blackColor] CGColor];
+    button.layer.shadowOpacity = 0.5;
+    button.layer.shadowOffset = CGSizeMake(3.0, 3.0);
+    button.layer.borderWidth = 1.0f;
+    button.layer.cornerRadius = 5.0f;
+    [button addTarget:self action:@selector(handleButtonClicked:) forControlEvents:UIControlEventTouchUpInside];
+    
+    [self.view setBackgroundColor:[UIColor cyanColor]];
+    [self.view addSubview:self.label];
+    [self.view addSubview:button];
     
 }
+
+- (void)handleButtonClicked:(id)sender {
+    UIButton *button = (UIButton *)sender;
+    if (button.isSelected) {
+        button.selected = NO;
+        self.label.text = @"請點擊下方按鈕";
+    } else {
+        button.selected = YES;
+        self.label.text = @"Hello World!!! Amber AppTeam";
+    }
+}
+
+//- (void)handleButtonClicked:(UIButton *)sender {
+//    if (sender.isSelected) {
+//        sender.selected = NO;
+//        self.label.text = @"請點擊下方按鈕";
+//    } else {
+//        sender.selected = YES;
+//        self.label.text = @"Hello World!!! Amber AppTeam";
+//    }
+//}
 
 
 @end
